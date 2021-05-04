96f87022bff776de601bc61f6930da533ee905be
green <greenn.lab@gmail.com>
2021-05-04 17:43:49 +0900

    vuetify for vue3

--- README.md
+++ README.md
@@ -1,24 +1,2 @@
-# taste-vuetify3
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+# summary
+아직 vuetify 에서 vue3 는 이르당...
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <v-app>
+      <v-container>
+        <v-col
+          :style="{ border: '1px solid' }"
+          v-for="n in 3"
+          :key="n"
+          sm="6"
+          md="4"
+        >
+          {{ n }}
+        </v-col>
+      </v-container>
+      <h1>
+        This is an about page123
+        <v-btn>
+          <v-icon left>mdi-minus-circle</v-icon>
+          Accept
+        </v-btn>
+        {{ test }}
+      </h1>
+    </v-app>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const test = ref('')
+</script>
